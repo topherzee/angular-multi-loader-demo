511d6e34d8f1a5d9b09478ce78fa2d105cea4144
Christopher Zimmermann <topher@dandelion.org>
2017-06-17 16:55:54 +0200

    Tidy up.

--- src/app/multi-loader.ts
+++ src/app/multi-loader.ts
@@ -1,68 +1,50 @@
 import {
-    enableProdMode,
-    Injector,
     ApplicationRef,
     NgModuleRef,
-    NgZone,
-    NgModuleFactory,
-    ReflectiveInjector,
-    ApplicationInitStatus,
-    SystemJsNgModuleLoader
+    ApplicationInitStatus
 } from '@angular/core';
 
 import {Container} from './container';
 
-
 export class MultiLoader {
 
   private DEFAULT_HABITAT_SELECTOR = 'data-component';
-
   private componentSelector: string;
 
-  constructor(public appModule: NgModuleRef<any>,
-              public container: Container) {
-                // Set dom component selector
-
-            this.componentSelector = this.DEFAULT_HABITAT_SELECTOR;
-
+  constructor(public appModule: NgModuleRef<any>, public container: Container) {
+    // Set dom component selector
+    this.componentSelector = this.DEFAULT_HABITAT_SELECTOR;
   }
 
-
   public initialize(): void {
-    let componentsWrapper = 'body';
-    let content = document.querySelector(componentsWrapper);
 
-    if (content) {
-      let componentElements: NodeListOf<Element> =
+    let componentElements: NodeListOf<Element> =
         document.querySelectorAll(`[${this.componentSelector}]`);
 
-      this.bootstrapComponents(componentElements);
-    }
+    this.bootstrapComponents(componentElements);
   }
 
 
   public bootstrapComponents(elements: NodeListOf<Element>): void {
 
-    for (var i = 0; i < elements.length; ++i) {
-      console.log("el i:" + i);
+    const appRef = this.appModule.injector.get(ApplicationRef);
+    const initStatus = this.appModule.injector.get(ApplicationInitStatus);
+    initStatus.donePromise.then(() => {
 
-      const el: Element= elements[i];
-      const id = el.getAttribute("id");
-      const elCompName = el.getAttribute(this.componentSelector);
-      console.log("el comp:" + elCompName + " id:" + id);
-      const component = this.container.resolve(elCompName);
+      for (var i = 0; i < elements.length; ++i) {
+        // Get the right component for this element.
+        const el: Element= elements[i];
+        const id = el.getAttribute("id");
+        const elCompName = el.getAttribute(this.componentSelector);
+        const component = this.container.resolve(elCompName);
 
-      const appRef = this.appModule.injector.get(ApplicationRef);
-      const initStatus = this.appModule.injector.get(ApplicationInitStatus);
-
-      initStatus.donePromise.then(() => {
         // Get the component factory and create it
         const compFactory = this.appModule.componentFactoryResolver
             .resolveComponentFactory(component);
 
         compFactory["factory"].selector = "#" + id;
         appRef.bootstrap(compFactory);
-      });
-    }
+      }
+    });
   }
 }
